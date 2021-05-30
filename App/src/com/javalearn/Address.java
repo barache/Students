b5a0c70a806468a760195ec189259bc8db2fac9f
Barache <kbarache@gmail.com>
2021-05-30 10:38:29 -0400

    Added new Attributs Student class and created address class

---
+++ App/src/com/javalearn/Address.java
@@ -0,0 +1,77 @@
+package com.javalearn;
+
+public class Address {
+
+	private int addressId;
+	private String streetName;
+	private String city;
+	private String province;
+	private String country;
+	private String postalCode;
+	
+	public Address() {
+	}
+
+	public Address(int addressId, String streetName, String city, String province, String country, String postalCode) {
+		this.addressId = addressId;
+		this.streetName = streetName;
+		this.city = city;
+		this.province = province;
+		this.country = country;
+		this.postalCode = postalCode;
+	}
+
+	public int getAddressId() {
+		return addressId;
+	}
+
+	public void setAddressId(int addressId) {
+		this.addressId = addressId;
+	}
+
+	public String getStreetName() {
+		return streetName;
+	}
+
+	public void setStreetName(String streetName) {
+		this.streetName = streetName;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getProvince() {
+		return province;
+	}
+
+	public void setProvince(String province) {
+		this.province = province;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+	
+	
+	
+	
+	
+	
+}
--- App/src/com/javalearn/Student.java
+++ App/src/com/javalearn/Student.java
@@ -5,15 +5,23 @@ public class Student {
 	private int id;
 	private String firstName;
 	private String lastName;
+	private String email;
+	private String phone;
+	private Address address;
 	
 	public Student() {
 	}
 
-	public Student(int id, String firstName, String lastName) {
+
+	public Student(int id, String firstName, String lastName, String email, String phone, Address address) {
 		this.id = id;
 		this.firstName = firstName;
 		this.lastName = lastName;
+		this.email = email;
+		this.phone = phone;
+		this.address = address;
 	}
+
 
 	public int getId() {
 		return id;
@@ -37,6 +45,36 @@ public class Student {
 
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getPhone() {
+		return phone;
+	}
+
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+
+	public Address getAddress() {
+		return address;
+	}
+
+
+	public void setAddress(Address address) {
+		this.address = address;
 	}
 	
 	
